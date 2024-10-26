a32edc0fbfebb71bd725fcf26f56d1bba1867a47
kairi <risa-muraoka@freee.co.jp>
2024-10-27 00:51:14 +0900

    認証情報をベタ打ちにしてみる

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
 
 // 環境変数からデータベースの接続情報を取得
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+const sequelize = new Sequelize('yourdbname', 'youruser', 'yourpassword', {
+    host: 'db',
+    port: 5432,
     dialect: 'postgres', // PostgreSQLを指定
 });
 
